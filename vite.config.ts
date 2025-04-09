8ac789a2a1827be443ef457d6f098b66d6fcc32c
VerkhoturovIP <verkhoturov314@gmail.com>
2025-04-09 16:46:35 +0300

    optimase build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
+    plugins: [
+        react(),
+        tsconfigPaths(),
+        /*
+        visualizer({
+            open: true,
+            filename: 'stats.html',
+        }),
+        */
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                chunkFileNames: 'assets/[name]-[hash].js',
+                manualChunks: (id) => {
+                    if (id.includes('chakra-ui')) return 'chakra-ui';
+                    if (id.includes('node_modules')) return 'libs';
+                },
+            },
+        },
+    },
 });
